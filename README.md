242f92def23cc4ba2ca57304c7d29b4d556b811f
OTerrien <oterrien@neuf.fr>
2018-12-14 23:51:58 +0100

    update docker

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 # docker-test-standalone
 
 This repo aims to test interactions with DockerHub and DockerCloud
+
+docker-machine ip --> 192.168.99.100
+docker images --> 5924aafc7c08
+docker run -it -p 192.168.99.100:8080:8080 docker 5924aafc7c08
+
+docker run -it 5924aafc7c08 /bin/bash 
+
--- src/main/java/com/ote/test/Controller.java
+++ src/main/java/com/ote/test/Controller.java
@@ -1,18 +1,15 @@
 package com.ote.test;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class Controller {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/ping")
-    public ResponseEntity<String> push() {
-        return new ResponseEntity("Service is started", HttpStatus.OK);
+    @GetMapping(value = "/ping")
+    public String ping() {
+        return "Service is started";
     }
 }
